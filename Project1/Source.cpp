f398bc26efc796aa6dfc59280f9c1517fe48dae5
Максим Пилипенко <@local.itstep>
2018-11-20 22:05:05 +0600

    Добавьте файлы проекта.

---
+++ Project1/Source.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+	int d, p, r, s;
+
+	d = 20;
+
+	p = 3.14;
+
+	s = (p / 4) * (d * d);
+
+	printf("result (s): %d\n", s);
+
+
+	system("pause");
+}
+/*#include <iostream>
+#include <stdio.h>
+int main()
+{
+	int r, R, s, p;
+
+	r = 3;
+
+	R = 4;
+
+	p = 3.14;
+
+	s = p * (R*R - r * r);
+
+	printf("result (s): %d\n", s);
+
+
+	system("pause");
+}
+/*#include <iostream>
+#include <stdio.h>
+int main()
+{
+	int a, b, d, p;
+
+	a = 3;
+
+	b = 4;
+
+	p = 2*(a + b);
+
+	d = sqrt((a * a) + (b * b));
+
+	printf("result (p): %d\n", p);
+
+	printf("result (d): %d\n", d);
+
+	system("pause");
+}
+/*#include <iostream>
+#include <stdio.h>
+int main()
+{
+	int a, b, c, p;
+
+	a = 3;
+
+	b = 4;
+
+	c = sqrt((a * a) + (b * b));
+
+	p = a + b + c;
+
+
+
+	printf("result (p): %d\n", p);
+
+	system("pause");
+}
+/*#include <iostream>
+#include <stdio.h>
+int main()
+{
+	int a, b, c;
+
+	a = 3;
+
+	b = 4;
+
+	c = sqrt(a * a + b * b);
+
+	printf("result (c): %d\n", c);
+
+	system("pause");
+}
+
+/*#include <iostream>
+#include <stdio.h>
+int main()
+{
+	int a, b, result;
+
+	a = 20;
+
+	b = 2;
+
+	result = a + b;
+
+	printf("result(a+b): %d\n", result);
+
+	result = a - b;
+
+	printf("result(a-b): %d\n", result);
+
+	result = a / b;
+
+	printf("result(a/b): %d\n", result);
+
+	result = a * b;
+
+	printf("result(a*b): %d\n", result);
+
+	system("pause");
+}
+
+/*#include <iostream>
+#include <stdio.h>
+int main()
+{
+	int d, P, s;
+
+	 P = 3.14;
+
+	 d = 5;
+
+	 s = P / 4 * d*d;
+
+
+	printf("result: %d\n", );
+
+	system("pause");
+}
+/*
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+	int r, P, c;
+
+	P = 3.14;
+
+	r = 5;
+
+	c = 2 * P * r;
+
+
+	printf("result: %d\n", c);
+
+	system("pause");
+}
+*/
+/*#include <iostream>
+#include <stdio.h>
+int main()
+{
+	int a, b, result;
+
+	result = a + b; 
+
+	printf("result(a+b): %d\n", c);
+
+	result = a - b;
+
+	printf("result(a-b): %d\n", c);
+
+	result = a / b;
+
+	printf("result(a/b): %d\n", c);
+
+    result = a * b;
+
+	printf("result(a*b): %d\n", c);
+
+	system("pause");
+}*/
